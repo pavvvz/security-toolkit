40bcc4ceefd6abfe528a7772396246c5c3db8e5f
pavvvz <mattiap128129@gmail.com>
2025-09-30 11:41:32 +0200

    fixed target directory

--- rust-backend/target/debug/deps/rust_backend-cbe5aeb321bb8767.d
+++
@@ -1,6 +0,0 @@
-/home/pav/Coding/security-toolkit/rust-backend/target/debug/deps/rust_backend-cbe5aeb321bb8767.d: src/main.rs src/portscanner.rs
-
-/home/pav/Coding/security-toolkit/rust-backend/target/debug/deps/librust_backend-cbe5aeb321bb8767.rmeta: src/main.rs src/portscanner.rs
-
-src/main.rs:
-src/portscanner.rs:
--- rust-backend/target/debug/rust-backend.d
+++
@@ -1,1 +0,0 @@
-/home/pav/Coding/security-toolkit/rust-backend/target/debug/rust-backend: /home/pav/Coding/security-toolkit/rust-backend/src/main.rs /home/pav/Coding/security-toolkit/rust-backend/src/portscanner.rs
